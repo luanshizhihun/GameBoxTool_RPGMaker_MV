a083b7f69e71fb4fc541cb602c6a9f250f6dde15
luanshizhihun <yangbeibo@gmail.com>
2016-11-26 13:33:40 +0800

    title change
    完善title类，实现背景图和背景装饰图的更换

--- GBT_titleScene.js
+++ GBT_titleScene.js
@@ -1,16 +1,70 @@
+//=============================================================================
+// GTB_titleScene.js
+//=============================================================================
+
+/*:
+ * @plugindesc 修改封面样式.
+ * @author YTB
+ *
+ * @param Background
+ * @desc 背景图名称.
+ * @default $dataSystem.title1Name
+ *
+ * @param Background secont
+ * @desc 背景装饰图.
+ * @default $dataSystem.title2Name
+ *
+ * @param Position X
+ * @desc 标题框坐标X.
+ * @default 0
+ *
+ * @param Position Y
+ * @desc 标题框坐标y.
+ * @default 0
+ *
+ * @help 帮助开发游戏菜单得插件
+ */
+
+/*:cn
+ * @plugindesc 修改封面样式.
+ * @author YTB
+ *
+ * @param Background
+ * @desc 背景图名称.
+ * @default 0
+ *
+ * @help 帮助开发游戏菜单得插件
+ */
+
 (function(){
 
 	var parameters = PluginManager.parameters('GBT_titleScene');
 
-	var background = Number(parameters['Background'] || 0);
+    var PositionX = Number(parameters['Position X'] || 0);
+    var PositionY = Number(parameters['Position Y'] || 0);
 
-	var mGBT_titleScene_updatePlacement = Window_TitleCommand.prototype.updatePlacement;
+	var mGBT_titleCommand_updatePlacement = Window_TitleCommand.prototype.updatePlacement;
     Window_TitleCommand.prototype.updatePlacement = function() {
-        mGBT_titleScene_updatePlacement.call(this);
+        mGBT_titleCommand_updatePlacement.call(this);
         // this.x += offsetX;
         // this.y += offsetY;
-        this.x = 100;
-        this.y = 100;
-        this.setBackgroundType(background);
+        this.x = PositionY;
+        this.y = PositionY;
+    };
+})();
+
+(function(){
+    var parameters = PluginManager.parameters('GBT_titleScene');
+
+    var backgroundImg = parameters['Background'] || $dataSystem.title1Name;
+    var backgroundImg2 = parameters['Background secont'] || $dataSystem.title2Name;
+
+    var mGBT_titleScene_createBackground = Scene_Title.prototype.createBackground;
+    Scene_Title.prototype.createBackground = function() {
+        mGBT_titleScene_createBackground.call(this);
+        this._backSprite1 = new Sprite(ImageManager.loadTitle1(backgroundImg));
+        this._backSprite2 = new Sprite(ImageManager.loadTitle2(backgroundImg2));
+        this.addChild(this._backSprite1);
+        this.addChild(this._backSprite2);
     };
 })();
